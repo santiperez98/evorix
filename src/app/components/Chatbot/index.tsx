4701f68261417a07594494b19db660be71e68e7a
santiperez98 <santip19982022@gmail.com>
2025-05-16 22:48:48 -0300

    fix bug

--- src/app/components/Chatbot/index.tsx
+++ src/app/components/Chatbot/index.tsx
@@ -1,4 +1,4 @@
-import { chatbotEngine } from './chatbotEngine';
+import { chatbotEngine } from './ChatbotEngine';
 
 // Simulación de uso:
 const userInput = 'Hola, quiero saber sobre desarrollo web';
